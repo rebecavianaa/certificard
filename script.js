64580680b8df94f3af69e00a0c272777d53f8e9e
Rebeca Viana <101978177+rebecavianaa@users.noreply.github.com>
2022-03-20 15:36:17 -0300

    adicionei os arquivos do certificard

---
+++ script.js
@@ -0,0 +1,11 @@
+// Desafios desta aula!
+//Terminar todo o design que está no figma (disponível abaixo)
+//Adicionar suas redes sociais de forma que quem clique consiga abrí-las //
+
+//embed para onseguir os links dessa maneira //
+
+function mudaTema() {
+	document.body.classList.toggle("dark");
+}
+
+// entra no github -> aperta no + no canto superior da pagina -> aperta em new repository -> escolhe o nome do repository -> nao clicar em nada na pagina, so colocar o nome certificard e prosseguir -> apertar em uploading an existent file -> no codepen, aperta em export e depois export.zip ->
